1e759a5b96592316a63e7c2d1ce6258bee8ab6bb
Kemal Özyön <kemalozyon51@gmail.com>
2025-01-10 16:18:55 +0300

    LG3 Q3 solved

---
+++ LG3/Q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    double value = 78.3456;
+    printf("%.0f\n",value);
+    printf("%.5f\n",value);
+    printf("%.8f\n",value);
+    printf("%9.1f\n",value);
+    printf("%.8f\n",value);
+    printf("%.2f\n",value);
+    printf("%6.0f\n",value);
+}
